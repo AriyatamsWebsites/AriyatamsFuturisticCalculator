7f975622dfd08a355a2dec544abfe7e9ff369430
AriyatamsWebsites <158802326+AriyatamsWebsites@users.noreply.github.com>
2024-02-04 14:06:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let displayValue = '0';
+
+function updateDisplay() {
+    document.getElementById('display').innerText = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    updateDisplay();
+}
+
+function deleteLast() {
+    displayValue = displayValue.slice(0, -1);
+    if (displayValue === '') {
+        displayValue = '0';
+    }
+    updateDisplay();
+}
+
+function appendSymbol(symbol) {
+    if (displayValue === '0' && symbol !== '.') {
+        displayValue = symbol;
+    } else {
+        displayValue += symbol;
+    }
+    updateDisplay();
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue).toString();
+    } catch (error) {
+        displayValue = 'Error';
+    }
+    updateDisplay();
+}
+
+updateDisplay();
